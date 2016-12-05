87011b54726809de24c3959a3e4751fb051f5753
Marc Litchfield <marclitchfield@gmail.com>
2016-12-04 19:28:24 -0800

    Remove logic to render child ideas. These will be on their own page

--- facets/ideas/app/components/idea.jsx
+++ facets/ideas/app/components/idea.jsx
@@ -11,7 +11,6 @@ export default class Idea extends React.Component {
     };
 
     this.toggleChildren = this.toggleChildren.bind(this);
-    this.renderChildren = this.renderChildren.bind(this);
   }
 
   toggleChildren() {
@@ -20,28 +19,16 @@ export default class Idea extends React.Component {
     });
   }
 
-  renderChildren() {
-    return 
-      this.state.childrenVisible ?
-      (<div className="relationships">
-        {sortedChildren.map(rel => 
-          <div className="relationship" key={rel.idea.id}>
-            <span className="predicate">{rel.predicate}</span>
-            <span className="body">{rel.idea.body}</span>
-          </div>
-          )}
-      </div>) : null;
-  }
-
   render() {
     const created = this.props.idea.created || Date.now();
     const sortedChildren = _.sortBy(this.props.relationships, (rel) => rel.idea.created);
     return (
-      <div className="idea" onClick={this.toggleChildren}>
-        <Link to={"/idea/" + this.props.idea.id}>{this.props.idea.body}</Link>
-        <div className="created">{moment(created).fromNow()}</div>
-        {this.renderChildren()}
-      </div>
+      <Link to={"/idea/" + this.props.idea.id}>
+        <div className="idea">
+          <div className="body">{this.props.idea.body}</div>
+          <div className="created">{moment(created).fromNow()}</div>
+        </div>
+      </Link>
     );
   }
 }
