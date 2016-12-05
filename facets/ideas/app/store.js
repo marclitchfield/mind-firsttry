7000a9488c479b0fe5e2e52623045e28210fe4ed
Marc Litchfield <marclitchfield@gmail.com>
2016-12-04 23:22:09 -0800

    Don't need redux-thunk yet

--- facets/ideas/app/store.js
+++ facets/ideas/app/store.js
@@ -1,9 +1,8 @@
 import { applyMiddleware, createStore } from "redux";
-import thunk from "redux-thunk";
 import promise from "redux-promise-middleware";
 import logger from "redux-logger";
 import reducer from "./reducer";
 
-const middleware = applyMiddleware(thunk, promise(), logger());
+const middleware = applyMiddleware(promise(), logger());
 
 export default (initialState) => createStore(reducer, initialState, middleware);
