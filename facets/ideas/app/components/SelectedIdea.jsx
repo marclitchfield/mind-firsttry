b67ee72e8d691e6bd4e0945ee75166fa1938b574
Marc Litchfield <marclitchfield@gmail.com>
2016-12-16 21:55:20 -0800

    Started on idea delete

--- facets/ideas/app/components/SelectedIdea.jsx
+++ facets/ideas/app/components/SelectedIdea.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router";
-import { MenuList, MenuItem, MenuButton, Dropdown } from "react-menu-list"
+import { MenuList, MenuItem, MenuButton, Dropdown } from "react-menu-list";
+import Modal from "react-modal";
 import IdeaType from "./IdeaType";
 import IdeaList from "./IdeaList";
 import IdeaSubmit from "./IdeaSubmit";
@@ -13,6 +14,9 @@ const NEW = "new";
 export default class SelectedIdea extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      deleteModalIsOpen: false
+    };
     this.route = this.route.bind(this);
     this.renderSelection = this.renderSelection.bind(this);
     this.renderSubmitNew = this.renderSubmitNew.bind(this);
@@ -20,6 +24,10 @@ export default class SelectedIdea extends React.Component {
     this.handleSubmitNew = this.handleSubmitNew.bind(this);
     this.handleSubmitEdit = this.handleSubmitEdit.bind(this);
     this.handleCancel = this.handleCancel.bind(this);
+    this.confirmDeleteIdea = this.confirmDeleteIdea.bind(this);
+    this.openDeleteConfirmModal = this.openDeleteConfirmModal.bind(this);
+    this.closeDeleteConfirmModal = this.closeDeleteConfirmModal.bind(this);
+    this.renderDeleteConfirmModal = this.renderDeleteConfirmModal.bind(this);
   }
 
   render() {
@@ -32,6 +40,7 @@ export default class SelectedIdea extends React.Component {
             ? this.renderEditor(selectedIdea, showType) 
             : this.renderSelection(selectedIdea, showType) }
         { this.route() === NEW ? this.renderSubmitNew() : null }
+        { this.renderDeleteConfirmModal() }
         <div className="children">
           <IdeaList ideas={selectedIdea.children} />
         </div>
@@ -83,12 +92,37 @@ export default class SelectedIdea extends React.Component {
     this.props.router.replace(`/idea/${this.props.selectedIdea.id}`);
   }
 
+  confirmDeleteIdea() {
+    this.openDeleteConfirmModal();
+  }
+
+  openDeleteConfirmModal() {
+    this.setState({ deleteModalIsOpen: true });
+  }
+
+  closeDeleteConfirmModal() {
+    this.setState({ deleteModalIsOpen: false });
+  }
+
+  renderDeleteConfirmModal() {
+    return (
+      <Modal
+        contentLabel="Confirm idea deletion" 
+        isOpen={this.state.deleteModalIsOpen} 
+        onRequestClose={this.closeDeleteConfirmModal}
+      >
+        <div>Are you sure you want to delete this idea</div>
+        <button onClick={this.closeDeleteConfirmModal} className="button">Cancel</button>
+      </Modal>
+    );
+  }
+
   renderOptionsMenu(selectedIdea) {
     return (
       <Dropdown>
         <MenuList>
           <MenuItem><Link to={`/idea/${selectedIdea.id}/edit`} className="button secondary">Edit</Link></MenuItem>
-          <MenuItem><Link to="/" className="button alert">Delete</Link></MenuItem>
+          <MenuItem><div onClick={this.confirmDeleteIdea} className="button alert">Delete</div></MenuItem>
         </MenuList>
       </Dropdown>
     );
